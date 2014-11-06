7f641a2aed61ae36f32c8327e0a3f13df8a0e080
Mingfei <yanmf@microsoft.com>
2014-11-06 15:54:58 -0800

    Delete SMF for PR

--- SMF for PR Token Auth/Source Code/microsoft-smf-src-2.2012.0605.0/Core/Offline/OfflineTaskType.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Microsoft.SilverlightMediaFramework.Core.Offline
-{
-    public enum OfflineTaskType
-    {
-        DownloadFile = 0,
-        ProcessManifest,
-        QueueChunkList
-    }
-}
--- SMF for PR Token Auth/Source Code/microsoft-smf-src-2.2012.0605.0/Core/TemplateDefinitions/VolumeControl/VolumeControlTemplateParts.cs
+++
@@ -1,9 +0,0 @@
-namespace Microsoft.SilverlightMediaFramework.Core.TemplateDefinitions
-{
-    internal static class VolumeControlTemplateParts
-    {
-        internal const string SliderElement = "SliderElement";
-        internal const string BaseElement = "BaseElement";
-        internal const string ExpandingElement = "ExpandingElement";
-    }
-}
