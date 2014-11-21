49f4b9eb591f158c32b082ae74b09854b77ed8b6
Mingfei <yanmf@microsoft.com>
2014-11-21 10:58:02 -0800

    Windows8AESPlugin

---
+++ Win8AESPlugin/SampleDownloaderPlugin/KeyLoader.cs
@@ -0,0 +1,96 @@
+﻿namespace CloudVideoClient.Xaml.Crypto
+{
+	using System;
+	using System.Collections.Generic;
+	using System.IO;
+	using System.Net;
+	using System.Net.Http;
+	using System.Runtime.Serialization;
+	using System.Runtime.Serialization.Json;
+	using System.Text;
+	using System.Threading;
+	using System.Threading.Tasks;
+    using Windows.Data.Json;
+	
+	[DataContract]
+	public class KeyIdData
+	{
+		[DataMember]
+		public string KeyId { get; set; }
+	}
+
+	public sealed class KeyLoader
+	{
+		private static ManualResetEvent allDone = new ManualResetEvent(false);
+
+		public static async Task<byte[]> LoadKey(Uri keyUri, string keyId)
+		{
+            //var keyIdData = new KeyIdData()
+            //{
+            //    KeyId = keyId,
+            //};
+
+            string authToken = "PUT_IN_YOUR_TOKEN_HERE";
+           
+            byte[] response = GetDeliveryKey(keyUri, authToken).Result;
+
+			return response;
+		}
+
+		private static void GetRequestStreamCallback(IAsyncResult asynchronousResult)
+		{
+			HttpWebRequest request = (HttpWebRequest)asynchronousResult.AsyncState;
+
+			// End the operation
+			request.EndGetRequestStream(asynchronousResult);
+			allDone.Set();
+		}
+
+
+		private static async Task<byte[]> GetDeliveryKey(Uri keyDeliveryUri, string token)
+		{
+			HttpWebRequest request = (HttpWebRequest)WebRequest.Create(keyDeliveryUri);
+
+			request.Method = "POST";
+			request.ContentType = "text/xml";
+			if (!string.IsNullOrEmpty(token))
+			{
+				request.Headers["Authorization"] = token;
+			}
+
+			request.BeginGetRequestStream(new AsyncCallback(GetRequestStreamCallback), request);
+			allDone.WaitOne();
+		
+
+			var response = await request.GetResponseAsync();
+			return GetDeliveryKey(response);
+		}
+
+		private static byte[] GetDeliveryKey(WebResponse response)
+		{
+			var stream = response.GetResponseStream();
+			if (stream == null)
+			{
+				throw new NullReferenceException("Response stream is null");
+			}
+
+			var buffer = new byte[256];
+			var length = 0;
+			while (stream.CanRead && length <= buffer.Length)
+			{
+				var nexByte = stream.ReadByte();
+				if (nexByte == -1)
+				{
+					break;
+				}
+				buffer[length] = (byte)nexByte;
+				length++;
+			}
+			//response.Close();
+
+			var key = new byte[length];
+			Array.Copy(buffer, key, length);
+			return key;
+		}
+	}
+}
---
+++ Win8AESPlugin/SampleDownloaderPlugin/MainPage.xaml.cs
@@ -0,0 +1,86 @@
+﻿using CloudVideoClient.Xaml.Crypto;
+using Microsoft.Media.AdaptiveStreaming;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace SampleDownloaderPlugin
+{
+
+    public class SampleCostants
+    {
+        public const string URL = "PUT_IN_STREAMING_URL";
+
+    }
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private MediaExtensionManager extensions = new MediaExtensionManager();
+        private Windows.Foundation.Collections.PropertySet propertySet = new Windows.Foundation.Collections.PropertySet();
+        private IAdaptiveSourceManager adaptiveSourceManager;
+        private AdaptiveSource adaptiveSource = null;
+        private AdaptiveSourceStatusUpdatedEventArgs adaptiveSourceStatusUpdate;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            // Gets the default instance of AdaptiveSourceManager which manages Smooth 
+            //Streaming media sources.
+            adaptiveSourceManager = AdaptiveSourceManager.GetDefault();
+            // Sets property key value to AdaptiveSourceManager default instance.
+            // {A5CE1DE8-1D00-427B-ACEF-FB9A3C93DE2D}" must be hardcoded.
+            propertySet["{A5CE1DE8-1D00-427B-ACEF-FB9A3C93DE2D}"] = adaptiveSourceManager;
+
+            // Registers Smooth Streaming byte-stream handler for “.ism” extension and, 
+            // "text/xml" and "application/vnd.ms-ss" mime-types and pass the propertyset. 
+            // http://*.ism/manifest URI resources will be resolved by Byte-stream handler.
+            extensions.RegisterByteStreamHandler("Microsoft.Media.AdaptiveStreaming.SmoothByteStreamHandler", ".ism", "text/xml", propertySet);
+            extensions.RegisterByteStreamHandler("Microsoft.Media.AdaptiveStreaming.SmoothByteStreamHandler", ".ism", "application/vnd.ms-ss", propertySet);
+            extensions.RegisterSchemeHandler("Microsoft.Media.AdaptiveStreaming.SmoothSchemeHandler", "ms-sstr:", propertySet);
+
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+
+            //Comment the line below to disable downloader plugin
+           // adaptiveSourceManager.SetDownloaderPlugin(new AdaptiveStreamDecoder.DecoderDownloadPlugin());
+
+
+            adaptiveSourceManager.SetDownloaderPlugin(new DecoderDownloadPlugin());
+
+            myPlayer.AutoPlay = true;
+            myPlayer.Visibility = Windows.UI.Xaml.Visibility.Visible;
+            myPlayer.Source = new Uri(SampleCostants.URL, UriKind.Absolute);
+
+        }
+    }
+
+
+    
+    }
+
+
+
